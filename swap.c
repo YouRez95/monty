7bf9a69888230956d072764e7bc07eb3939737b0
YouRez95 <younesselalouani1995@gmail.com>
2023-09-21 23:50:29 +0100

    swap

--- swap.c
+++ swap.c
@@ -12,21 +12,18 @@
 void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
+	int data;
 
-	temp = *stack;
-	if (temp == NULL || temp->next == NULL)
+	(void) line_number;
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	temp = temp->next;
-	(*stack)->prev = temp;
-	(*stack)->next = temp->next;
-	if (temp->next)
-		temp->next->prev = (*stack);
-	temp->next = *stack;
-	temp->prev = NULL;
+	temp = *stack;
 
-	*stack = temp;
+	data = temp->n;
+	temp->n = temp->next->n;
+	temp->next->n = data;
 }
