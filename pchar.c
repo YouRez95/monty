cd5fed2e33fa5a33bdcd3d34c992e7a94e215ca1
YouRez95 <younesselalouani1995@gmail.com>
2023-09-23 23:43:02 +0100

    pchar

--- pchar.c
+++ pchar.c
@@ -21,7 +21,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 	}
 	if (temp->n > 127)
 	{
-		fprintf(stderr, "L%u: can,t pchar, value out of range\n", line_number);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (char)temp->n);
